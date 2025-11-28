8f952fc818adba32452eae875fa3cf548b9812f7
SUHO PARK <duglikespiano@gmail.com>
2025-11-29 06:43:22 +0900

    16 - REQUIRE

---
+++ 16-require/app.js
@@ -0,0 +1,7 @@
+// CommonJS modules uses keyword 'require' and ES6 modules uses 'import' to import data
+
+const os = require('os');
+
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
