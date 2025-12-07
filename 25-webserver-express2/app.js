2aa6bb30337435bffafa648d31cc1f4e10544ce7
SUHO PARK <duglikespiano@gmail.com>
2025-12-07 21:48:36 +0900

    25 - WEBSERVER EXPRESS2

---
+++ 25-webserver-express2/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+app.get(/^\/(index\.html)?$/, (req, res) => {
+	console.log('check');
+	res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
