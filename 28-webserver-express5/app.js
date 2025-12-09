b924228fea32faddcaa2d8f72847b6392d05607a
SUHO PARK <duglikespiano@gmail.com>
2025-12-10 00:01:49 +0900

    28 - WEBSERVER EXPRESS5

---
+++ 28-webserver-express5/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+app.get(/^\/(index(\.html)?)?$/, (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get(/^\/new-page(\.html)?$/, (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get(/^\/old-page(\.html)?$/, (req, res) => {
+	res.redirect(301, '/new-page.html'); //302 by default
+});
+
+app.use((req, res, next) => {
+	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
