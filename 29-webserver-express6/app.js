cf6c73fc8ef6ff2ed30e431027f8134a929519f2
SUHO PARK <duglikespiano@gmail.com>
2025-12-11 00:10:34 +0900

    29 - WEBSERVER EXPRESS6

---
+++ 29-webserver-express6/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+app.get(/^\/(index(\.html)?)?$/, (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get(
+	/^\/(hello(\.html)?)?$/,
+	(req, res, next) => {
+		console.log('hello');
+		next();
+	},
+	(req, res) => {
+		console.log(req.path);
+		res.send('Hello world');
+	}
+);
+
+app.get(/^\/new-page(\.html)?$/, (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get(/^\/old-page(\.html)?$/, (req, res) => {
+	res.redirect(301, '/new-page.html'); //302 by default
+});
+
+app.use((req, res, next) => {
+	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
